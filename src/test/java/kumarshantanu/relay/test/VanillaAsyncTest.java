d3e9af18bb30d736c8bb5005e6a00b67d304c037
Shantanu Kumar <kumar.shantanu@gmail.com>
2012-12-03 00:43:29 +0530

    remove reference to Worker interface from vanilla test

--- src/test/java/kumarshantanu/relay/test/VanillaAsyncTest.java
+++ src/test/java/kumarshantanu/relay/test/VanillaAsyncTest.java
@@ -6,8 +6,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
-import kumarshantanu.relay.Worker;
-
 import org.junit.Test;
 
 public class VanillaAsyncTest {
@@ -15,22 +13,12 @@ public class VanillaAsyncTest {
 	@Test
 	public void test() {
 		final AtomicLong counter = new AtomicLong();
-		//fail("Not yet implemented");
 		assertTrue("Test started", true);
-		final Worker<String, String> worker = new Worker<String, String>() {
-			public boolean isIdempotent() {
-				return true;
-			}
-			public String execute(String req) {
-				return req;
-			}
-		};
 		final ExecutorService threadPool = Executors.newFixedThreadPool(8);
 		Runnable sender = new Runnable() {
 			public void run() {
 				threadPool.execute(new Runnable() {
 					public void run() {
-						worker.execute("Hello World!");
 						counter.incrementAndGet();
 					}
 				});
